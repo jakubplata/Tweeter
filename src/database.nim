253c9fa133bca74125f38dcdcddd730146f696b6
Jakub Plata <plata.jakub@gmail.com>
2019-11-29 21:17:07 +0100

    database tests

--- src/database.nim
+++ src/database.nim
@@ -20,6 +20,34 @@ type
     time*: Time
     msg*: string
 
+proc close*(database: Database) =
+  database.db.close()
+
+proc setup*(database: Database) =
+  database.db.exec(sql"""
+    CREATE TABLE IF NOT EXISTS User(
+    username text PRIMARY KEY
+  );
+  """)
+  database.db.exec(sql"""
+  CREATE TABLE IF NOT EXISTS Following(
+    follower text,
+    followed_user text,
+    PRIMARY KEY (follower, followed_user)
+    FOREIGN KEY (follower) REFERENCES User(username),
+    FOREIGN KEY (followed_user) REFERENCES User(username)
+  );
+  """)
+
+  database.db.exec(sql"""
+  CREATE TABLE IF NOT EXISTS Message(
+    username text,
+    time integer,
+    msg text NOT NULL,
+    FOREIGN KEY (username) REFERENCES User(username)
+  );
+  """)
+
 proc post*(database: Database, message: Message) =
   if message.msg.len > 140:
     raise newException(ValueError, "Message has to be less than 140 characters.")
@@ -43,14 +71,13 @@ proc findUser*(database: Database, username: string, user: var User): bool =
 
   let following = database.db.getAllRows(
     sql"SELECT followed_user FROM Following WHERE follower = ?;", username)
-  )
   user.following = @[]
   for row in following:
     if row[0].len != 0:
         user.following.add(row[0])
-  return True
+  return true
 
-proc findMessages*(database: Database, usernames: seq[string]), limit = 10): seq[Message] =
+proc findMessages*(database: Database, usernames: seq[string], limit = 10): seq[Message] =
   result = @[]
   if usernames.len == 0: return
   var whereClause = " WHERE "
@@ -60,9 +87,9 @@ proc findMessages*(database: Database, usernames: seq[string]), limit = 10): seq
       whereClause.add(" or ")
   
   let messages = database.db.getAllRows(
-    sql"SELECT username, time, msg FROM Message" & whereClause &
-    "ORDER BY time DESC LIMIT " & $limit), usernames
-  )
+    sql("SELECT username, time, msg FROM Message" &
+      whereClause &
+      "ORDER BY time DESC LIMIT " & $limit), usernames)
   for row in messages:
     result.add(Message(username: row[0], time: fromSeconds(row[1].parseInt),
       msg: row[2]))
